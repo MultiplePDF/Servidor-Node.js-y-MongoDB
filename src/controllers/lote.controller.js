a07092970f184760450086c32a49c0bb0ca12ded
Arkarok <47455232+Arkarok@users.noreply.github.com>
2023-03-22 21:18:51 -0500

    method downloadLotes

--- src/controllers/lote.controller.js
+++ src/controllers/lote.controller.js
@@ -41,9 +41,9 @@ export const createLote = async (req, res) => {
 export const uploadLotes = async (req, res) => {
   const { idUsuario } = req.body;
   try {
-    const lotes = await Lote.find({ idUsuario: idUsuario });
+    const lotes = await Lote.find({ idUsuario });
 
-    if (!lotes) {
+    if (lotes.length == 0) {
       return res
         .status(404)
         .json({ message: "No existe lotes para este usuario" });
@@ -58,15 +58,32 @@ export const uploadLotes = async (req, res) => {
 export const uploadfiles = async (req, res) => {
   const { idUsuario } = req.body;
   try {
-    const lotes = await Lote.find({ idUsuario: idUsuario });
+    const lotes = await Lote.find({ idUsuario });
 
-    if (!lotes) {
+    if (lotes.length == 0) {
       return res
         .status(404)
         .json({ message: "No existe archivos para este usuario" });
     }
 
     return res.status(200).json(lotes[0].archivos);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const downloadLote = async (req, res) => {
+  const { idLote } = req.body;
+  try {
+    const lotes = await Lote.findById({ idLote });
+
+    if (lotes.length == 0) {
+      return res
+        .status(404)
+        .json({ message: "No existe ese lote para este usuario" });
+    }
+
+    return res.status(200).json(lotes[0].rutaLote);
   } catch (error) {
     console.log(error);
   }
